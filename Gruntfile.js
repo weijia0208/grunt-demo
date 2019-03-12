98757102002aeca8dcd9107898b7d586ed609a76
weijia0208 <10024752762@qq.com>
2019-03-12 15:40:20 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite: {
+      all: {
+        src: 'images/*.png',
+        dest: 'dist/spritesheet.png',
+        destCss: 'dist/sprites.css'                                     
+      }     
+    }
+      
+  });
+
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.registerTask('default', ['sprite']);
+
+};
